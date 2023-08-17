a0d8df68126e0df42fb10d0fe39810fcd5be4c3c
Brian McClain <brianmmcclain@gmail.com>
2023-08-17 11:34:18 -0400

    Add Terraform configuration

---
+++ modules/01-fundamentals/src/terraform.tf
@@ -0,0 +1,19 @@
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.69.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
